fdc5cf95c6308cda7668525d4b775357f49ea77c
DilanRamirez <drramirezra@miners.utep.edu>
2020-08-02 17:14:00 -0700

    Updates

---
+++ precache-manifest.85670610f0c3188481652c1c37887dfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d484b5decc1485aa7789ba163a3c06",
+    "url": "/condiciones-atmosfericas/index.html"
+  },
+  {
+    "revision": "9ac0a780e3fd5db2b8a9",
+    "url": "/condiciones-atmosfericas/static/css/main.e47ea929.chunk.css"
+  },
+  {
+    "revision": "cb867ae0fda36e31e139",
+    "url": "/condiciones-atmosfericas/static/js/2.cfbbafb5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/condiciones-atmosfericas/static/js/2.cfbbafb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac0a780e3fd5db2b8a9",
+    "url": "/condiciones-atmosfericas/static/js/main.2f1ecd5d.chunk.js"
+  },
+  {
+    "revision": "f87669e418643700cc0e",
+    "url": "/condiciones-atmosfericas/static/js/runtime-main.40ad2840.js"
+  },
+  {
+    "revision": "8aac0ff26649cef5d8ad115f89174030",
+    "url": "/condiciones-atmosfericas/static/media/cloud.8aac0ff2.png"
+  },
+  {
+    "revision": "242c13568f6a2181caccfadd867c6501",
+    "url": "/condiciones-atmosfericas/static/media/sun.242c1356.png"
+  }
+]);
